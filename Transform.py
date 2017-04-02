7ace788c3961412543985c50cba70ac47e7f96ba
Codrut Erdei <codrut@hackermail.com>
2017-04-02 21:48:05 +0300

    Add files via upload

--- Transform.py
+++ Transform.py
@@ -16,12 +16,9 @@ def distance(xa, ya, xb, yb):
 
 def showPath(endNode):
     i = endNode
-    print("path:", end=" ")
     while nodes[i].prev is not None:
-        print(nodes[i], end=", ")
         if nodes[i].x < len(maze) and nodes[i].y: maze[nodes[i].x][nodes[i].y] = 127
         i = nodes[i].prev.num
-    print(nodes[i])
     if nodes[i].x < len(maze) and nodes[i].y: maze[nodes[i].x][nodes[i].y] = 127
 
 
@@ -75,21 +72,16 @@ def decode(path):
     for j in range(1, len(maze) - 1):
         for i in range(1, len(maze[j]) - 1):
 
-            # checking every pixel
-            if maze[i][j] != 0:
-                # checking neighbours
-                if maze[i - 1][j] == 0 or maze[i][j - 1] == 0 or \
-                   maze[i + 1][j] == 0 or maze[i][j + 1] == 0:
+            
+            # creating a temporary node
+                temp = Node()
+                temp.x = j                    
+                temp.y = i
 
-                    # creating a temporary node
-                    temp = Node()
-                    temp.x = j
-                    temp.y = i
-
-                    # if the node wasn't found before add it into the set of nodes
-                    if found_before(temp) is False:
-                        nodes.append(temp)
-                        maze[i][j] = 200
+                # if the node wasn't found before add it into the set of nodes
+                if found_before(temp) is False:
+                    nodes.append(temp)
+                    maze[i][j] = 200
 
     # creating the adjacence matrix
     field = Maze(len(nodes))
@@ -133,13 +125,84 @@ def decode(path):
             if maze[node.x][node.y + j] == 0:
                 break
 
-    print([x for x in field.adjacence])
 
 
-decode("maze.png")
 
-search_djikstra(0)
 
-showPath(nodes[len(nodes) - 1].num)
+# new shit {
+
+def decodeAtor(path):
+    global nodes
+    # reading the image in grayscale
+    global maze
+    maze = cv2.imread(path, 0)
+
+    # iterating through the pixels
+    for j in range(1, len(maze) - 1):
+        for i in range(1, len(maze[j]) - 1):
+
+            # checking every pixel
+            if maze[i][j] != 0:
+                temp = Node()
+                temp.x = j
+                temp.y = i
+
+            # if the node wasn't found before add it into the set of nodes
+                if found_before(temp) is False:
+                    nodes.append(temp)
+                    maze[i][j] = 200
+
+    # creating the adjacence matrix
+    field = Maze(len(nodes))
+
+    for node in nodes:
+        i = 0
+        j = 0
+        while True:
+
+            i += 1
+
+            if maze[node.x + i][node.y] == 255:
+                if get_node(node.x + i, node.y) and found_before(get_node(node.x, node.y)):
+
+                    get_node(node.x + i, node.y).prev = get_node(node.x, node.y)
+
+                    field.add(node.num,
+                              get_node(node.x + i, node.y).num,
+                              distance(node.x, node.y, get_node(node.x + i, node.y).x, get_node(node.x + i, node.y).y))
+
+
+            if node.x + i >= len(maze) - 1:
+                break
+            if maze[node.x + i][node.y] == 0:
+                break
+
+        while True:
+
+            j += 1
+
+            if maze[node.x][node.y + j] != 0:
+                if get_node(node.x, node.y + j):
+
+                    get_node(node.x, node.y + j).prev = get_node(node.x, node.y)
+
+                    field.add(node.num,
+                              get_node(node.x, node.y + j).num, 
+                              distance(node.x, node.y, get_node(node.x, node.y + j).x, get_node(node.x, node.y + j).y))
+
+            if node.y + j >= len(maze) - 1:
+                break
+            if maze[node.x][node.y + j] == 0:
+                break
+
+
+
+# new shit }
+
+decodeAtor("maze.png")
+
+search_djikstra(0)  
+
+showPath(nodes[len(nodes) - 2].num)
 
 cv2.imwrite("imgu.png", maze)
